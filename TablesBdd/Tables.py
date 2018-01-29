6167ea8fbf96e209f0b1d58b9c7e86b7e5434e3a
sebastien <sebastien.mariaux@gmail.com>
2018-01-29 10:16:04 +0100

    Reorganizing code, keywords functions set up

---
+++ TablesBdd/Tables.py
@@ -0,0 +1,120 @@
+"""
+Collects tweets by keywords
+"""
+
+#! /usr/bin/env python3
+# coding: utf-8
+
+from sqlalchemy import Column, Integer, String, Boolean#, ForeignKey, Text, DateTime, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class KeyWords(Base):
+    """
+    Table to store keywords to be researched on twitter API
+    """
+    __tablename__ = 'keywords'
+    key = Column(String, primary_key=True)
+    nb_query = Column(Integer)
+    active = Column(Boolean)
+
+    def __init__(self, key):
+        self.key = key
+        self.nb_query = 0
+        self.active = True
+
+    def __repr__(self):
+        return "<keyword : {}>".format(self.key)
+
+
+class Users(Base):
+    """
+    User-specific data from tweets
+    """
+    __tablename__ = u'users'
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String)
+    user_screen_name = Column(String)
+    user_location = Column(String)
+    user_descr = Column(String)
+    user_lang = Column(String)
+    user_followers_count = Column(Integer)
+    user_verified = Column(Boolean)
+    user_timezone = Column(String)
+    user_friends_count = Column(Integer)
+
+    def __init__(self, user_data):
+        self.user_id = user_data['user_id']
+        self.user_name = user_data['user_name']
+        self.user_screen_name = user_data['user_screen_name']
+        self.user_location = user_data['user_location']
+        self.user_descr = user_data['user_descr']
+        self.user_lang = user_data['user_lang']
+        self.user_followers_count = user_data['user_followers_count']
+        self.user_verified = user_data['user_verified']
+        self.user_timezone = user_data['user_timezone']
+        self.user_friends_count = user_data['user_friends_count']
+
+    def __repr__(self):
+        return "<User : {} - {}>".format(self.user_id, self.user_name)
+
+
+class Tweets(Base):
+    __tablename__ = u'Tweets'
+    tweet_id = Column(Integer, primary_key=True)
+    query = Column(String)
+    inserted_date = Column(String)
+    date = Column(String)
+    month = Column(String)
+    year = Column(String)
+    truncated = Column(Boolean)
+    urls = Column(String)
+    hashtags = Column(String)
+    user_mentions_name = Column(String)
+    user_mentions_id = Column(String)
+    text = Column(String)
+    user_id = Column(Integer)
+    user_name = Column(String)
+    language = Column(String)
+    place = Column(String)
+    sensitive = Column(Boolean)
+    reply_to_tw = Column(Integer)
+    reply_to_user = Column(Integer)
+    reply_to_user_name = Column(String)
+    is_rt = Column(Boolean)
+    source_tw = Column(String)
+    media_type = Column(String)
+    media_url = Column(String)
+    media_nb = Column(Integer)
+    json_output = Column(String)
+
+    def __init__(self, tweet_data):
+        self.tweet_id = tweet_data['tweet_id']
+        self.query = tweet_data['query']
+        self.inserted_date = time.strftime('%Y_%m_%d', time.localtime())
+        self.date = tweet_data['date']
+        self.month = tweet_data['month']
+        self.year = tweet_data['year']
+        self.truncated = tweet_data['truncated']
+        self.urls = tweet_data['urls']
+        self.hashtags = tweet_data['hashtags']
+        self.user_mentions_name = tweet_data['user_mentions_name']
+        self.user_mentions_id = tweet_data['user_mentions_id']
+        self.text = tweet_data['text']
+        self.user_id = tweet_data['user_id']
+        self.user_name = tweet_data['user_name']
+        self.language = tweet_data['language']
+        self.place = tweet_data['place']
+        self.sensitive = tweet_data['sensitive']
+        self.reply_to_tw = tweet_data['reply_to_tw']
+        self.reply_to_user = tweet_data['reply_to_user']
+        self.reply_to_user_name = tweet_data['reply_to_user_name']
+        self.is_rt = tweet_data['is_rt']
+        self.source_tw = tweet_data['source_tw']
+        self.media_type = tweet_data['media_type']
+        self.media_url = tweet_data['media_url']
+        self.media_nb = tweet_data['media_nb']
+        self.json_output = tweet_data['json_output']
+
+
--- TweetCollect.py
+++ TweetCollect.py
@@ -4,52 +4,149 @@ Collects tweets by keywords
 
 #! /usr/bin/env python3
 # coding: utf-8
+import os
+import re
+import logging as lg
+import sqlalchemy
+from sqlalchemy import func
+from TablesBdd.Tables import KeyWords, Tweets, Users, Base
+from connect import TwitterApi
 
-from sqlalchemy import Column, Integer, String#, ForeignKey, Text, DateTime, Float, Boolean
-from connect import SqlaSession, TwitterApi
+lg.basicConfig(level=lg.INFO)
+api = TwitterApi.main()
 
-
-API = TwitterApi.main()
-session, Base = SqlaSession.main()
-#import pdb; pdb.set_trace()
-
-
-class KEYWORDS(Base):
+class Database:
     """
-    Sqlite table to store research keys
+    Database to store data from Twitter
     """
-    __tablename__ = u'keywords'
-    key = Column(String, primary_key =True)
-    nb_query = Column(Integer)
 
-    def __init__(self):
-        self.nb_query = 0
 
-    def add_keys(session=s, keywords = [], hashtags=[]):
+    def __init__(self, filename='twitter_data'): #default database name = data
+        self.name = filename
+        directory = os.path.dirname(os.path.abspath(__file__))
+        bdd_file = os.path.join(directory, "data", self.name)
+        bdd_file = "sqlite:///"+bdd_file+".sqlite"
+        engine = sqlalchemy.create_engine(bdd_file, echo=False)
+        Session = sqlalchemy.orm.sessionmaker(bind=engine)
+        self.session = Session()
+        Base.metadata.create_all(engine)
+        self.all_keywords = []
+        self.active_keywords = []
+        self.inactive_keywords = []
+
+    def add_keywords(self, keywords_list):
         """
-        Ajoute des mots clés à rechercher
+        Set the list of keywords to look up on Twitter
         """
-        for h in hashtags:
-            h = h.replace("#","")
-            h = "%23"+h
-            keywords.append(h)
+        for key in keywords_list:
+            key = re.sub('^#', "%23", key) #remplace # par '%23'
+            key_already_exists = self.session.query(KeyWords).filter_by(key=key)
+            if not key_already_exists.all():
+                new = KeyWords(key)
+                self.session.add(new)
+                lg.info("New keyword added to the table 'keywords' : '%s'", key)
+            elif key_already_exists.one().active is False:
+                key_to_reactivate = key_already_exists.one()
+                key_to_reactivate.active = True
+                lg.info("Key '%s' status passed to 'active'", key)
+        self.session.commit()
 
-        for k in keywords:
-            test = s.query(KEYWORDS).filter_by(key=k).all()
-            if not test:
-                new = KEYWORDS(k)
-                s.add(new)
-        s.commit()
+    def get_kw(self):
+        """
+        Create/update lists of keywords (all / active / inactive)
+        """
+        self.all_keywords = [el.key for el in self.session
+                             .query(KeyWords.key)]
+        self.active_keywords = [el.key for el in self.session
+                                .query(KeyWords.key)
+                                .filter_by(active=True)
+                                .order_by(KeyWords.nb_query)]
+        self.inactive_keywords = [el.key for el in self.session
+                                  .query(KeyWords.key)
+                                  .filter_by(active=False)]
+        #import pdb ; pdb.set_trace()
+
+    def desactivate_keywords(self, keywords):
+        """
+        Pass status of keywords to inactive when removed from the text file
+        """
+        keywords = [re.sub('^#', "%23", key) for key in keywords]
+        for key in self.active_keywords:
+            if key not in keywords:
+                key_to_desactivate = self.session.query(KeyWords).filter_by(key=key).one()
+                key_to_desactivate.active = False
+                self.session.commit()
+                lg.info("Key %s status passed to 'inactive'", key)
+
+    def get_limits(self, key):
+        """
+        Get the newest and oldest tweet already collected for a given keyword
+        """
+        max_id = self.session.query(func.max(Tweets.tweet_id)).filter_by(query=key).one()
+        min_id = self.session.query(func.min(Tweets.tweet_id)).filter_by(query=key).one()
+        return min_id, max_id
+
+    def query_api(self, key, oldest_tweets=True):
+        lg.info('Searching tweets for keywords %s', key)
+        min_id, max_id = self.get_limits(key)
+
+        continu = True
+        i=0
+        while continu:
+            i+=1
+            lg.info("Recherche pour le mot clé %s : recherche numéro %s", key, i)
+            try:
+                if oldest_tweets == True:
+                    res = api.search(q=key,
+                                     count=100,
+                                     result_type="recent",
+                                     max_id=min_id-1,
+                                     include_entities=True,
+                                     tweet_mode='extended')
+                else:
+                    res = api.search(q=key,
+                                     count=100,
+                                     result_type="recent",
+                                     since_id=max_id+1,
+                                     include_entities=True,
+                                     tweet_mode='extended')
+                if res['statuses']:
+                    lg.info("Nombre de tweets : %s", len(res['statuses']))
+                    min_id = enregistrement(res, key)
+                    continu = True
+                    oldest_tweets = True
+                    lg.debug("min_id : %s", min_id)
+                else:
+                    lg.info("Plus de résultat pour le mot clé '%s' ; passage au mot clé suivant", key)
+                    continu = False
+
+            except TwythonRateLimitError:
+                lg.warning("Twitter limit reached. Waiting 15 minutes before moving to next keyword")
+                time.sleep(900)
+                break
+
+            #######  UPDATE NUMBER OF QUERIES FOR A KEYWORD ######
+            key_upd = s.query(KEYWORDS).filter_by(key = k).one()
+            key_upd.nb_query += 1
+            s.commit()
 
 def main():
-    pass
-    #On défini la liste des mots clés à traiter
+    #Creating / connecting to Bdd
+    db = Database(filename='twitter_data')
 
-    #On lance la collecte
+    #Updating keywords list
+    with open('keywords.txt') as keywords_file:
+        keywords = ''.join(keywords_file.readlines()).replace(' ', '').split(',')
+    db.add_keywords(keywords)
+    db.get_kw()
+    db.desactivate_keywords(keywords)
+    db.get_kw() #Repeat this line to eliminate recently desactivated keys from query list
 
-    #ON récupère les infos et on les mets dans le format attendu
+    #Tweets Collection
+    for key in db.active_keywords:
+        db.query_api(key)
 
-    #On créé les objets correspondants
+
 
 
 if __name__ == '__main__':
